45b018c76b85f94293bc4efc16feb1a40cc5ca7a
SireneNra <sirene.nouira@cyclable.com>
2026-01-21 09:18:32 +0100

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://blog-affiliation.com',
+  site: 'https://sirenenouira.github.io',
   base: '/Blog-perso/',
   output: 'static',
   build: {
